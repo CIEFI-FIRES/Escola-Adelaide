48cc87d102dc31072c1c7c2fab34e3fdf2aa0c4e
Breno Fiussen <150976795+BrenoFiussen@users.noreply.github.com>
2024-08-18 19:40:06 -0300

    Update bot_o_buzzer1.ino

--- Exemplos - ESP32/bot_o_buzzer1.ino
+++ Exemplos - ESP32/bot_o_buzzer1.ino
@@ -1,3 +1,15 @@
+//Exemplo básico de controle de tom utilizando um sensor digital.
+
+//Este script em C++ configura os pinos de um microcontrolador, lê o estado de um pino de entrada digital e, com base nessa leitura, emite um som em uma frequência específica. É um exemplo simples de como controlar um sinal sonoro utilizando um botão ou sensor digital.
+
+//Funcionalidade:
+
+//Configura os pinos 2 e 7 como entrada e saída, respectivamente.
+//O pino 7 é configurado para emitir um sinal sonoro (buzzer).
+//O pino 2 é utilizado como entrada digital para detectar um estado lógico (HIGH ou LOW).
+//Se o pino 2 for lido como LOW, o script aciona um tom de 698 Hz no pino 7 por 100 milissegundos.
+//A leitura é atualizada a cada 10 milissegundos para melhorar o desempenho da simulação.
+//
 // C++ code
 //
 void setup()
